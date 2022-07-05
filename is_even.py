3bcd6e58f6fc1b2926a4f8dd3f5fa021c9ac7e3c
Sokhibjon <90791234+soxibdavurov@users.noreply.github.com>
2022-07-05 22:55:56 +0500

    Update is_even.py

--- is_even.py
+++ is_even.py
@@ -1,8 +1,7 @@
 #Create a variable "number" and assign it a integer value.
 
 number=4
-print(number%2+1)
-number = 9
-print(0)
+k=number%2
+print(abs((k-1)))
 
 #Print 1 if the number is even, otherwise print 0.
