feb6e3fe5fb8c25ff7b346e732dffa5fb20542c4
Sokhibjon <soxib.davurov@gmail.com>
2022-07-05 20:29:20 +0500

    is_even

--- is_even.py
+++ is_even.py
@@ -1,3 +1,9 @@
 #Create a variable "number" and assign it a integer value.
 
+number = int(input("Enter a number: "))
+if (number % 2) == 0:
+   print(1)
+else:
+    print(0)
+
 #Print 1 if the number is even, otherwise print 0.
