719af73421bb86dd33edd3a4286762019805451d
Sokhibjon <90791234+soxibdavurov@users.noreply.github.com>
2022-07-06 00:11:35 +0500

    Update number_even.py

--- number_even.py
+++ number_even.py
@@ -2,19 +2,20 @@
 
 #Create a variable "var_int" and assign it a four-digit integer value.
 
-var_int = int(1234)
+var_int = int(input())
 a=var_int//1000
 b=var_int%1000//100
 c=var_int%100//10
 d=var_int%10
 
 if(a%2==0):
-    print(1)
+    print(a)
 if(b%2==0):
-    print(2)
+    print(b)
 if(c%2==0):
-    print(3)
+    print(c)
 if(d%2==0):
-    print(4)
+    print(d)
+
 
 #Print the number of even digits in the variable "var_int".
