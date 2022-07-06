43d03cd5e88b0070ca70205d2489ce1f577e6a5e
Sokhibjon <90791234+soxibdavurov@users.noreply.github.com>
2022-07-06 11:24:33 +0500

    Update sum_odd.py

--- sum_odd.py
+++ sum_odd.py
@@ -4,4 +4,17 @@
 
 #Create a variable "sum_even" and assign it 0.
 
+var_int = int(input("Enter a four digit number: "))
+sum_odd = 0
+
+
+if(var_int > 999):
+    while var_int > 0:
+        b = var_int % 10
+        if(b % 2 != 0 ):
+            sum_odd+=b
+        var_int//=10
+    print("Sum of odd digits ", sum_odd)
+else:
+    print("Not a four digit number")
 #Find the sum of the odd digits in the variable "var_int".
