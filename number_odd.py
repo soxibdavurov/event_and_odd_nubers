527e1e5b51ad2d4c7c0b44e32e70bd02b95e89ed
Sokhibjon <90791234+soxibdavurov@users.noreply.github.com>
2022-07-06 12:08:22 +0500

    Update number_odd.py

--- number_odd.py
+++ number_odd.py
@@ -2,4 +2,15 @@
 
 #Create a variable "var_int" and assign it a four-digit integer value.
 
+var_int = int(1234)
+
+if(var_int > 999):
+    while var_int > 0:
+        b = var_int %10
+        if(b % 2 != 0 ):
+            print(b)
+        var_int//=10
+    
+else:
+    print("Not a four digit number")
 #Print the number of odd digits in the variable "var_int".
